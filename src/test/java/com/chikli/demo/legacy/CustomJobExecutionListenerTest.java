fef624d018f41424144936fdd7a3d2276b8b10dc
Nayan Hajratwala <nayan@chikli.com>
2015-05-28 13:00:48 -0400

    added second test

--- src/test/java/com/chikli/demo/legacy/CustomJobExecutionListenerTest.java
+++ src/test/java/com/chikli/demo/legacy/CustomJobExecutionListenerTest.java
@@ -1,12 +1,11 @@
 package com.chikli.demo.legacy;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import javax.swing.JPanel;
-
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.MDC;
 import org.springframework.batch.core.JobExecution;
@@ -15,22 +14,46 @@ import org.springframework.batch.core.JobParameters;
 
 public class CustomJobExecutionListenerTest {
 
+	private CustomJobExecutionListener listener;
+	private JobExecution jobExecution;
+	private JobInstance jobInstance;
+	private JobParameters jobParameters;
+
+	@Before
+	public void setup() {
+		listener = new CustomJobExecutionListener();
+
+		jobExecution = mock(JobExecution.class);
+		jobInstance = mock(JobInstance.class);
+		when(jobExecution.getJobInstance()).thenReturn(jobInstance);
+		jobParameters = new JobParameters();
+	}
+
 	@Test
 	public void blankFilePath() {
-		CustomJobExecutionListener listener = new CustomJobExecutionListener();
-
-		JobExecution jobExecution = mock(JobExecution.class);
-		JobInstance jobInstance = mock(JobInstance.class);
-		when(jobExecution.getJobInstance()).thenReturn(jobInstance);
 		when(jobInstance.getJobName()).thenReturn("Fred");
-		JobParameters jobParameters = new JobParameters();
 		String filePath = "";
 		String processDate = "2015-10-10";
 
 		String logfile = listener.setupLogfile(jobExecution, jobParameters, filePath, processDate);
 
-		assertThat(logfile, is("Fred2015-10-10"));
-		assertThat(MDC.get("inputFileName"), is("Fred2015-10-10"));
+		assertLogfile(logfile, "Fred2015-10-10");
+	}
+
+	@Test
+	public void nonBlankFilePath() {
+		when(jobInstance.getJobName()).thenReturn("Fred");
+		String filePath = "/tmp/blah.txt";
+		String processDate = "2015-10-10";
+
+		String logfile = listener.setupLogfile(jobExecution, jobParameters, filePath, processDate);
+
+		assertLogfile(logfile, "blah.txt");
+	}
+
+	private void assertLogfile(String logfile, String expectedFilename) {
+		assertThat(logfile, is(expectedFilename));
+		assertThat(MDC.get("inputFileName"), is(expectedFilename));
 	}
 
 }
